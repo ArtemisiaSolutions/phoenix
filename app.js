87ef726938f4fabed27e3f493d678634dbc0fedb
Eric Hostalery <hostalerye@gmail.com>
2013-02-04 12:28:59 +0100

    temperature layer works

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express')
+    require("express-namespace")
   , routes = require('./routes')
   , http = require('http')
   , path = require('path')
--- public/javascripts/phoenix.js
+++ public/javascripts/phoenix.js
@@ -3,12 +3,12 @@ var baseUrl = "";
 function getData(map, control, type, date, altitude)
 {
 	$.ajax({
-		type: "POST",
+		type: "GET",
 		url: baseUrl+"data/temperature/toto/tata",
 		dataType: "json",
 		success: function(res) {
 			/* Verifier contenu */
-			console.log(res);
+			console.log(JSON.stringify(res))
 
 			/* Test ajout d'un layer */
 			var heatmapLayer3 = L.TileLayer.heatMap({
@@ -21,12 +21,12 @@ function getData(map, control, type, date, altitude)
 					0.6: "yellow",
 					1.0: "rgb(255,0,0)"
 				}
-			});
-			var data = res.data;
-			heatmapLayer3.addData(data);
-			control.addOverlay(heatmapLayer3, "heatmapPlayer3");
+			})
+			var data = res
+			heatmapLayer3.addData(data)
+			control.addOverlay(heatmapLayer3, "heatmapPlayer3")
 
-			map.addLayer(heatmapLayer3);
+			map.addLayer(heatmapLayer3)
 		}
 	});
 }
@@ -35,30 +35,30 @@ $(function() {
 
 	/* Fit screen height */
 	
-	$("#frame").css({'height' : $(window).height()});
-	$("#map").css({'height' : $(window).height() - $("#header").outerHeight()});
+	$("#frame").css({'height' : $(window).height()})
+	$("#map").css({'height' : $(window).height() - $("#header").outerHeight()})
 
 	/* Map */
 
-	var baselayer = L.tileLayer('http://{s}.tile.cloudmade.com/a2c2a163b7bb473e9ee42c9431d3671a/997/256/{z}/{x}/{y}.png',{styleId: 22677, attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'});
+	var baselayer = L.tileLayer('http://{s}.tile.cloudmade.com/a2c2a163b7bb473e9ee42c9431d3671a/997/256/{z}/{x}/{y}.png',{styleId: 22677, attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'})
 
 	var map = new L.Map('map', {
 		center: new L.LatLng(47.2738, 0.88855),
 		zoom: 6,
 		layers: [baselayer]
-	});
+	})
 
-	var control = L.control.layers();
-	control.addTo(map);
+	var control = L.control.layers()
+	control.addTo(map)
 	
 	/* Listeners */
 
 	$("#temperature").click(function() {
-		getData(map, control, "temperature", "null", "null");
-	});
+		getData(map, control, "temperature", "null", "null")
+	})
 
 	$("#wind").click(function() {
-		getData(map, control, "wind", "null", "null");
-	});
+		getData(map, control, "wind", "null", "null")
+	})
 
 });
